34cd5bc5c0d828aab75d498ec67dff4a984464a5
casey <caseymwangi31@gmail.com>
2024-03-20 12:01:32 +0300

    adding content

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = 'Casey';
+
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toLocaleUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
